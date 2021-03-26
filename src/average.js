22015947433af4540b4a630812a2ccd6675ac9b8
joao966 <joaooliveira966@gmail.com>
2021-03-26 15:19:15 -0300

    FIX: requisito 1

--- src/average.js
+++ src/average.js
@@ -13,17 +13,19 @@
 */
 
 const average = (array) => {
-  let resultado = 0;
-  if (array.length == 0){
+  let soma = 0;
+  let media = 0;
+  if (array.length === 0) {
     return undefined;
   }
-  if (typeof(array.length) !== 'number'){
-    return undefined;
-  }
-  for (let i = 0; i < array.length; i += 1){
-    resultado += array[i];
-  }
-  return Math.parseInt(resultado / array.length)
+  for (let i = 0; i < array.length; i += 1) {
+    if (typeof array[i] !== 'number') {
+      return undefined;      
+    } 
+    soma += array[i];    
+  };
+  media = soma / array.length;
+  return Math.parseInt(media);
 };
 
 module.exports = average;
