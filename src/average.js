d33653023ef88ad0d962187c4b5efc4b724be38c
Nikolas <nikolas.silva1990@gmail.com>
2021-04-03 08:14:45 -0300

    Requisito 1 revisado

--- src/average.js
+++ src/average.js
@@ -16,12 +16,12 @@ const average = (arrayNumbers) => {
   // Caso a função receba um array vazio, o valor undefined deve ser retornado.
   if (arrayNumbers.length === 0) return undefined;
   // Caso a função receba algum valor não númerico, o valor undefined deve ser retornado.
-  let values = 0;
+  let checkedNumbers = 0;
   for (let index = 0; index < arrayNumbers.length; index += 1) {
     if (typeof arrayNumbers[index] !== 'number') {
       return undefined;
     }
-    values += arrayNumbers[index];
+    checkedNumbers += arrayNumbers[index];
   }
   // Retorna a média dos valores recebidos e todos os resultados devem ser arredondados para valores inteiros.
   return Math.round(arrayNumbers / arrayNumbers.length);
