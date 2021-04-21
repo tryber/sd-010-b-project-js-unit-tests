cbe25b37466ea46de8c6701f830ab2a7d275380c
LucasPeixoto <peixotolucas516@gmail.com>
2021-04-21 18:47:41 -0300

    jest path removido de numbers.spec

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -1,20 +1,17 @@
+
 /* eslint-disable max-len */
 /* eslint-disable no-unused-vars */
 
-const { expect } = require('@jest/globals');
 const assert = require('assert');
-const { number } = require('yargs');
 const numbers = require('../src/numbers');
 
 /*
   A função numbers recebe um array (tamanho variável) e retorna true se todos os parâmetros forem do tipo 'number' e false caso contrário.
-
   Parâmetros:
     - Um array. Exemplos: [1, 2]; [1, 2, 3, 4, 5]; [1, 2, 'a']; [].
   Comportamento:
     - numbers([2, 3, 4]); // Retorna: true
     - numbers([2, 'errado', 5]); // Retorna: false
-
   OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
 
