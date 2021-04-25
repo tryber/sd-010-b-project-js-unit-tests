eec84313f64c342d1d740612f09520dbeabe14b9
Jonnes Bezerra <jonnes.bezerra@gmail.com>
2021-04-24 23:21:39 -0300

    RESTAURANT: removing unecessary lines

--- src/restaurant.js
+++ src/restaurant.js
@@ -79,17 +79,6 @@
 // soma o preço de todos checando-os no menu e retorna o valor somado acrescido de 10%. DICA: para isso,
 // você precisará varrer tanto o objeto da chave `food` quanto o objeto da chave `drink`.
 
-// const menu = {
-//   food: {
-//     coxinha: 3.9,
-//     sopa: 9.9,
-//   },
-//   drink: {
-//     agua: 3.9,
-//     cerveja: 6.9,
-//   },
-// };
-
 const restaurant = {};
 const itemOrdered = (ordered) => restaurant.consumption.push(ordered);
 
